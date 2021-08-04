798f5f78acce505d96acf57814b3676da3b44081
pyaesoneaung <pyaesoneaung.ucsy@gmail.com>
2021-08-04 16:44:15 +0630

    first commit

---
+++ src/test/java/com/psa/springwebfluxrest/SpringWebfluxRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psa.springwebfluxrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebfluxRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
